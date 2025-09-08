963fd946f3d1d25a5a86af082ce0aabffa589c79
agirton090 <agirton090@phm.k12.in.us>
2025-09-08 09:50:23 -0400

    Create DataStructures.py

---
+++ DataStructures.py
@@ -0,0 +1,11 @@
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+print(numbers[0])
+print(numbers[9])
+print(numbers[4])
+numbers.append(11)
+#Part 1: Lists
+1. #Create a list of 10 numbers.
+2. #Print the first, last, and middle number.
+3. #Add a new number to the end of the list.
+4. #Replace the third number with 99.
+5. #Loop through the list and print only the even numbers.

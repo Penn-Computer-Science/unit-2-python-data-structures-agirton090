983d7d04f672f744fb5ccdb149fa27459be05abd
agirton090 <agirton090@phm.k12.in.us>
2025-09-10 08:39:20 -0400

    Update DataStructures.py

--- DataStructures.py
+++ DataStructures.py
@@ -1,8 +1,3 @@
-numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-print(numbers[0])
-print(numbers[9])
-print(numbers[4])
-numbers.append(11)
 #Part 1: Lists
 1. #Create a list of 10 numbers.
 2. #Print the first, last, and middle number.

121f415eba6da169414ee3c42fc4d6f3d35e8930
agirton090 <agirton090@phm.k12.in.us>
2025-09-16 09:21:19 -0400

    Gradebook Manager
    ready to grade.

--- gradebookManager.py
+++ gradebookManager.py
@@ -17,16 +17,27 @@ def letter_grade(avg):
         return "F"
     
 def show_summary(gradebook):
+    print("----- Gradebook Summary -----")
+    averages = {}
+
     if not gradebook:
         print("Gradebook is empty.")
         return
-    
-    print("----- Gradebook Summary -----")
-    averages = {}
+
     for student, grades in gradebook.items():
         avg = average(grades)
         averages[student] = avg
-        print()
+        print(f"{student}: {avg:.2f} ({letter_grade(avg)}) ")
+
+        
+    if averages:
+        top_student = max(averages, key=averages.get)
+        print(f"Top Student: {top_student} with average {averages[top_student]:.2f}")
+
+    
+
+    
+    
 
 
 def main():
@@ -46,7 +57,7 @@ def main():
         print("5. View summary")
         print("6. Quit")
 
-        choice = input("Choose (1-6): ")
+        choice = input("Choose an option (1-6): ")
 
         if choice == "1":
             name = input("Student name: ")
@@ -54,8 +65,52 @@ def main():
                 print("That student already exists.")
             else:
                 grades = input("Enter grades separated by spaces: ")
-                gradebook[name] = [int(x) for x in grades.split()]
-                print("Added {name}.")
+                gradebook[name] = list(map(int, grades.split()))
+                print(f"Added {name}.")
+
+        elif choice == "2":
+            name = input("Enter student name: ")
+            if name not in gradebook:
+                print("Student not found.")
+            else:
+                grades = input("Enter grades separated by spaces: ")
+                gradebook[name].extend(list(map(int, grades.split())))
+                print("Grades added for {name}.")
+        
+        elif choice == "3":
+            name = input ("Enter student name to remove: ")
+            if name in gradebook:
+                del gradebook[name]
+                print(f"{name} removed.")
+            else:
+                print("Student not found.")
+        
+        elif choice == "4":
+            name = input("Enter student name: ")
+            if name in gradebook:
+                print(f"{name}'s grades {gradebook[name]}")
+                try:
+                    grade = int(input("Enter the grade to remove: "))
+                    gradebook[name].remove(grade)
+                    print(f"Removed {grade} from {name}.")
+                except ValueError:
+                    print("That grade was not found.")
+            else:
+                print("Student not found.")
+
+        elif choice == "5":
+            show_summary(gradebook)
+
+        elif choice == "6":
+            print("Exiting program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+main()
+
+        
+
 
 
 
